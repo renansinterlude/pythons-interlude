3e8c2bf3b658a99702d9568f7598b3178c06c74a
Renan Moreira <106031464+renansinterlude@users.noreply.github.com>
2023-12-16 21:46:33 -0300

    criação gerador de nome de bandas
    gerador de nome de bandas - dia 1
    
    inputs e concatenação

---
+++ day-1/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Bem vindo ao Gerador de Nome de Banda!")
+cidade = input("Em qual cidade você cresceu? ")
+animal = input("Qual o nome do seu animal de estimação? ")
+print("O nome da sua banda deveria ser " + cidade + " " + animal + "!")
